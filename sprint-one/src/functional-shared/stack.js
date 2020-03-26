6131341a479067465d14a842ca3069a944cdd59d
Nathaniel Torrez <mr.realestatenate@gmail.com>
2020-03-25 22:26:53 -0700

    Solved functional patterns, working on functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // will be our numeric keys
+  var size = 0
+  // this will store the elements of stack
+  var storage = {}
+  // this object can store the stack methods as we add them
+  var someInstance = {};
+  // we will now continiously extend the method object to contain all values passed into stackMethods
+
+  return someInstance
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    size++;
+    storage[size] = value ;
+  },
+
+  pop: function(){
+    var lastElement = storage[size];
+    size--;
+    delete storage[size];
+    return lastElement;
+  },
+
+  size: function(){
+    if (size < 0){
+      return 0
+    }
+    return size;
+  }
+};
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,35 @@
 var Stack = function() {
+
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = { };
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+     size++;
+     storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var lastElement = storage[size];
+    delete storage[size];
+    size--;
+    return lastElement;
   };
 
   someInstance.size = function() {
+
+    if (size < 0) {
+      return 0;
+    }
+    return size;
   };
 
   return someInstance;
 };
+var test = Stack()
+test.push(1)
+test.size()
+test.pop()
