758154d2772fe77f5277d63b3051aa1e213ac3ea
Nathaniel Torrez <mr.realestatenate@gmail.com>
2020-04-01 08:41:49 -0700

    working on binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,29 +1,43 @@
 var BinarySearchTree = function(value) {
-  var tree = Object.create(treePrototype);
-  tree.value = value;
-  tree.left = null;
-  tree.right = null;
-  return tree;
+  // save value to a variable
+  this.value = value;
+  //left property that stores smaller or equal to current value
+  this.left = undefined;
+  // right propterty that stores larger or equal to current value
+  this.right = undefined;
 };
-var treePrototype = {};
-
-treePrototype.insert = function(val) {
-  if (val < this.value){
-    if (this.left === null){
-      this.left = BinarySearchTree(val);
-    } else {
-      this.left.insert(val);
+BinarySearchTree.prototype.addNode = function(value) {
+  // check if value was smaller or larger than current value choose direction
+  var newTree = new BinarySearchTree(value);
+  if (value >= this.value) {
+    // if direction undefined place tree - Base case
+    if (this.right === undefined) {
+      // create a new instance of tree
+      this.right = newTree;
+      return newTree;
     }
-  } else if (val > this.value){
-   this.right = BinarySearchTree(val);
-  } else {
-    this.right.insert(val)
-  } else {
+    // Repeat process on child tree
+    this.right.addNode(value);
+  }
+  if (this.left === undefined) {
+    this.left = newTree;
+    return newTree;
+  }
+  this.left.addNode(value);
 
-  };
-}
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var testTree = new BinarySearchTree(10);
+console.log(testTree.value); //10
+testTree.addNode(20);
+console.log(testTree);
+testTree.addNode(30);
+console.log(testTree,testTree.right);
+
+// var anotherTestTree = new BinarySearchTree(5);
+// anotherTestTree.addNode();
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -11,14 +11,14 @@ setPrototype.add = function(item) {
 };
 
 setPrototype.contains = function(item) {
-  if(this._storage[item]){
+  if (this._storage[item]) {
     return true;
   }
   return false;
 };
 
 setPrototype.remove = function(item) {
-  delete this._storage[item]
+  delete this._storage[item];
 };
 
 /*
